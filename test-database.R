d24d3987153512f1e297eeade840360ee77ad666
Yusman Kamaleri <ybkamaleri@gmail.com>
2021-04-13 18:54:35 +0200

    Test Befolkning and Dode data from SSB

---
+++ test-database.R
@@ -0,0 +1,18 @@
+## Create database from Befolknings file
+OS <- Sys.info()["sysname"]
+
+osDrive <- switch(OS, Linux="/mnt/F", Windows="F:")
+orgPath <- "Forskningsprosjekter/PDB 2455 - Helseprofiler og til_/PRODUKSJON/ORGDATA/SSB"
+
+## SSB Befolkning
+befFile <- "BEFOLKNING/ORG/2022/G1a2021.csv"
+befPath <- file.path(osDrive, orgPath, befFile)
+
+library(data.table)
+
+befDT <- fread(befPath)
+
+## SSB Dode
+dodeFile <- "DODE_SSB/ORG/2021/G42019_v3.csv"
+dodePath <- file.path(osDrive, orgPath, dodeFile)
+dodeDT <- fread(dodePath, encoding = "Latin-1")
