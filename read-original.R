bed6273a6ad8e33739b24769dad3c3543a771487
YBK <ybkamaleri@users.noreply.github.com>
2021-05-12 17:38:14 +0200

    Feat: Read raw data with read_org function

--- read-original.R
+++ read-original.R
@@ -1,4 +1,9 @@
 ## Read raw data
-read_org <- function() {
-
+read_org <- function(file, arguments) {
+  orgfile <- substitute(file)
+  argsTxt <- paste0("data.table::fread(", orgfile, ",", arguments, ",", "encoding = 'Latin-1'",")")
+  DT <- eval(parse(text = argsTxt))
 }
+
+
+default_cols_names <- c("GEO", "AAR", "ALDER", "KJONN", "UTDANN", "LANDBAK", "SIVST")
