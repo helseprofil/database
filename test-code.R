923bf2a5df45a4a829eac947ffc74f04ad2cf801
YBK <ybkamaleri@users.noreply.github.com>
2021-06-14 23:46:16 +0200

    Expl: Add total for each columns

--- test-code.R
+++ test-code.R
@@ -37,6 +37,9 @@ dodePath <- file.path(osDrive, orgPath, dodeFile)
 dodeDT <- fread(dodePath, encoding = "Latin-1", skip = 1)
 dodeDT2 <- fread(dodePath, encoding = "Latin-1")
 
+## OBS!! SSB Dode has code 99999999 which has no name
+## in GRUNNKRETS downloaded with API
+
 ## Add geo levels
 dt2 <- copy(dodeDT2)
 dt <- copy(DT)
@@ -50,6 +53,10 @@ keepvar <- c("name", "validTo", "level", "grunnkr", "kommune", "fylke", "bydel")
 dt2[dt, on = c(GRUNNKRETS = "code"), (keepvar) := mget(keepvar)]
 setnames(dt2, grep("^Utdann", names(dt2), value = T), "utdanning")
 
+## OBS!! SSB Dode has code 99999999 which has no name
+## in GRUNNKRETS downloaded with API
+dt2[GRUNNKRETS == 99999999, name := "Uoppgitt grunnkrets"]
+
 dim(dt2)
 dim(dodeDT)
 
@@ -61,19 +68,57 @@ dt2[, .N, by = utdanning]
 sdt <- groupingsets(
   dt2[!is.na(fylke)],
   j = .(antall = sum(antall, na.rm = T)),
-  by = c("fylke", "utdanning"),
+  by = c("fylke", "utdanning", "KJOENN", "landb"),
   sets = list(
+    c("fylke", "utdanning", "KJOENN", "landb"),
+    c("fylke", "utdanning", "KJOENN"),
+    c("fylke", "utdanning", "landb"),
+    c("fylke", "KJOENN", "landb"),
     c("fylke", "utdanning"),
-    c("fylke"),
-    character(0)
+    c("fylke", "KJOENN"),
+    c("fylke", "landb"),
+    c("fylke")
   )
 )
 
-sdt[is.na(fylke), fylke := 0]
+sdt
+setnames(sdt, "fylke", "GEO")
+sdt[, aar := 2019]
+setcolorder(sdt, "aar")
+
+## total as 8888
+for (x in seq_len(ncol(sdt))) {
+  set(sdt, which(is.na(sdt[[x]])), j = x, value = 8888)
+}
+
+setkey(sdt, GEO)
+fwrite(sdt, "pivot_dode.csv", sep = ";")
+
 sdt[is.na(utdanning), utdanning := 8888] # total
 sdt
 
 ## use it as dynamic vars
+vars <- c("fylke", "KJOENN", "utdanning", "landb")
 vars <- c("fylke", "utdanning")
 sdt2 <- rollup(dt2[!is.na(fylke)], j = list(n = sum(antall, na.rm = T)), by = vars)
 sdt2
+
+sdt3 <- data.table::cube(dt2[!is.na(fylke)], j = list(n = sum(antall, na.rm = T)), by = vars)
+sdt3
+
+
+## Add total manually
+dt2
+dt3 <- dt2[!is.na(fylke), .(antall = sum(antall)), keyby = .(aar, fylke, KJOENN, utdanning, landb)]
+dt3
+dt_tot <- dt3[!is.na(fylke), .(antall = sum(antall, na.rm = T)), keyby = .(aar, fylke)]
+dt_tot
+dt_tot[, utdanning := 8888]
+dt_tot[, KJOENN := 8888]
+dt_tot[, landb := 8888]
+
+dt4 <- rbindlist(list(dt3, dt_tot), use.names = TRUE)
+setkey(dt4, fylke)
+setnames(dt4, "fylke", "GEO")
+dt4[1:60]
+fwrite(dt4, "dode_fylke.csv", sep = ";")
