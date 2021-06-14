4917125bc9bedd96d99db540fc1ea6a2fa292f7b
YBK <ybkamaleri@users.noreply.github.com>
2021-06-14 15:48:26 +0200

    Expl: Use groupsets functions to aggregate

--- test-code.R
+++ test-code.R
@@ -35,3 +35,45 @@ befDT <- fread(befPath)
 dodeFile <- "DODE_SSB/ORG/2021/G42019_v3.csv"
 dodePath <- file.path(osDrive, orgPath, dodeFile)
 dodeDT <- fread(dodePath, encoding = "Latin-1", skip = 1)
+dodeDT2 <- fread(dodePath, encoding = "Latin-1")
+
+## Add geo levels
+dt2 <- copy(dodeDT2)
+dt <- copy(DT)
+
+str(dt2)
+str(dt)
+
+dt[, code := as.integer(code)]
+
+keepvar <- c("name", "validTo", "level", "grunnkr", "kommune", "fylke", "bydel")
+dt2[dt, on = c(GRUNNKRETS = "code"), (keepvar) := mget(keepvar)]
+setnames(dt2, grep("^Utdann", names(dt2), value = T), "utdanning")
+
+dim(dt2)
+dim(dodeDT)
+
+dt2
+dt2[, .(antall = sum(antall)), by = .(kommune, SIVILSTAND)]
+
+dt2[, .N, by = utdanning]
+
+sdt <- groupingsets(
+  dt2[!is.na(fylke)],
+  j = .(antall = sum(antall, na.rm = T)),
+  by = c("fylke", "utdanning"),
+  sets = list(
+    c("fylke", "utdanning"),
+    c("fylke"),
+    character(0)
+  )
+)
+
+sdt[is.na(fylke), fylke := 0]
+sdt[is.na(utdanning), utdanning := 8888] # total
+sdt
+
+## use it as dynamic vars
+vars <- c("fylke", "utdanning")
+sdt2 <- rollup(dt2[!is.na(fylke)], j = list(n = sum(antall, na.rm = T)), by = vars)
+sdt2
