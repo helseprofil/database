08aa11c52b483a4f55d69d98ea10741b9f163066
YBK <ybkamaleri@users.noreply.github.com>
2021-07-21 18:02:44 +0200

    Feat: Use groupingsets with cube style

--- lesorg.R
+++ lesorg.R
@@ -1,5 +1,6 @@
 getwd()
-Rfiles <- c("misc.R", "parameter.R", "conn-db.R", "read-original.R")
+## Rfiles <- c("misc.R", "parameter.R", "conn-db.R", "read-original.R")
+Rfiles <- "conn-db.R"
 invisible(sapply(Rfiles, source))
 
 coEnv <- new.env()
@@ -35,9 +36,27 @@ AND tbl_Orgfile.IBRUKTIL = #9999-01-01#
 }
 
 dd <- get_raw("BEFOLKNING")
+tic()
 orgFile <- data.table::fread(file.path(coEnv$orgPath, dd$FILNAVN))
+toc()
+
+library(tictoc)
+library(future)
+plan(multisession)
+ff <- future({
+  orgFile <- data.table::fread(file.path(coEnv$orgPath, dd$FILNAVN))
+})
+ff2 <- value(ff)
+ff2
+rm(ff2)
+
+tic()
+ff3 %<-% data.table::fread(file.path(coEnv$orgPath, dd$FILNAVN))
+ff3
+toc()
 
 ## Manheader
+
 names(orgFile)
 dd
 
@@ -65,10 +84,71 @@ rename_col <- function(data = NULL, spec = NULL) {
 }
 
 dt <- rename_col(data = orgFile, spec = dd)
-
+dt
 
 ## Split LANDBK
 dt[, c("landb", "landf") := data.table::tstrsplit(LANDBK, "", fixed = TRUE)]
+dt
 
-
+## Aggregere
 dd$AGGRIGERE
+
+## aggrCols <- c("KJONN", "ALDER", "UTDANN", "SIVILSTAND", "landb", "landf")
+
+dt <- LesOrg("Dode")
+dt <- LesOrg("BEFOLKNING")
+aggCols <- setdiff(names(dt), c("GEO", "LANDBK", "VAL"))
+names(dt)
+
+## When lowest geo codes are grunnkrets, else should check what codes are in the rawdata
+geoDT <- DBI::dbGetQuery(coEnv$conn$dbconn, "SELECT * FROM tbl_Geo WHERE level = 'grunnkrets'")
+head(geoDT)
+setDT(geoDT)
+str(geoDT)
+geoDT[, code := as.integer(code)]
+delVar <- c("code", "level")
+keepVar <- setdiff(names(geoDT), delVar)
+keepVar
+dt
+dt[geoDT, on = c(GEO = "code"), (keepVar) := mget(keepVar)]
+
+aggr <- "kommune"
+
+DT <- data.table::groupingsets(
+  dt[!is.na(get(aggr))],
+  j = .(VAL = sum(VAL, na.rm = TRUE)),
+  by = c(aggr, aggCols),
+  sets = list(
+    c(aggr, aggCols),
+    c(aggr, "landb"),
+    c(aggr, "landf")
+  )
+)
+
+
+CJ(aggCols, aggCols)
+
+
+dtsam <- sample(1:nrow(dt), 10)
+dtmin <- dt[dtsam]
+dtmin
+dtmin[1:5, VAL := 2]
+dtmin
+aggCols <- c("fylke", "KJONN", "SIVILSTAND", "UTDANN")
+aggCols2 <- c("KJONN", "SIVILSTAND", "UTDANN")
+DTT <- data.table::cube(dtmin, j = list(antall = sum(VAL, na.rm = T)), by = aggCols)
+DTT[!is.na(fylke)]
+
+
+groupingsets(
+  dtmin,
+  j = .(antall = sum(VAL, na.rm = T)),
+  by = aggCols,
+  sets = list(
+    aggCols,
+    c("fylke", "KJONN", "SIVILSTAND"),
+    c("fylke", "KJONN", "UTDANN"),
+    c("fylke", "KJONN"),
+    c("fylke", "SIVILSTAND")
+  )
+)
